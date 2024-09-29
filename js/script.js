c4e01bc5c2767cfb3954adc19eaacdefaa108921
cryptic <Andr Cerqueira>
2024-09-29 22:18:01 +0100

    Changed the website, now it's a moving target

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-let d = new Date();
-document.body.innerHTML = "<p>Today's date is <strong>" + d + "</strong></p>";
+function show_image(src, width) {
+    let div = document.createElement("div");
+    div.style.position = "absolute";
+    div.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+    div.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+    div.onclick = function() { show_image("svgs/target.svg", width); div.remove(); };
+    let img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    div.appendChild(img);
+    document.body.appendChild(div);
+}
+
+show_image("svgs/target.svg", "50");
