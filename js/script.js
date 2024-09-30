a3bbde9cc7bc28f1db2f1ab7ddecb0e27ff6f671
cryptic <Andr Cerqueira>
2024-09-30 14:25:14 +0100

    Added a score

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
-function show_image(src, width) {
+function show_image(src, width, score) {
+    let h1 = document.createElement("h1");
+    h1.textContent = "Score = " + score;
+    h1.textContent = "Score = " + score;
     let div = document.createElement("div");
     div.style.position = "absolute";
-    div.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-    div.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
-    div.onclick = function() { show_image("svgs/target.svg", width); div.remove(); };
+    div.style.left = Math.floor(Math.random() * (0.8*window.innerWidth - 50) + 50) + "px";
+    div.style.top = Math.floor(Math.random() * (0.8*window.innerHeight - 50) + 50) + "px";
+    div.onclick = function() { show_image("svgs/target.svg", width, score + 1); div.remove(); h1.remove(); };
     let img = document.createElement("img");
     img.src = src;
     img.width = width;
     div.appendChild(img);
     document.body.appendChild(div);
+    document.body.appendChild(h1);
 }
 
-show_image("svgs/target.svg", "50");
+function show_score(score) {
+    let div = document.createElement("div");
+    let h1 = document.createElement("h1");
+    h1.textContent = "Score = " + score;
+    div.appendChild(h1);
+    document.body.appendChild(div);
+}
+
+show_image("svgs/target.svg", "50", 0);
